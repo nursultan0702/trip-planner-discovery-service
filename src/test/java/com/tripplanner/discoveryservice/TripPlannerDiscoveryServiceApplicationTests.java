3827ccea6c735d7e04a97b2f8d96f7d828d306c6
Nursultan Sadyk <nursultan.sadyk1995@gmail.com>
2024-04-16 11:42:42 +0500

    init commit

---
+++ src/test/java/com/tripplanner/discoveryservice/TripPlannerDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripplanner.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlannerDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
